13e92961eeb6798183c8238fb3aa96f256649ecd
ByteShadow-dev <shubham.behera2509@gmail.com>
2025-12-05 11:31:52 +0530

    5/12/25

---
+++ B_Tab_Closing.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define ll long long
+#define vi vector<int>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define ff first
+#define ss second
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+
+void solve() {
+    // Solution
+    int n, a, b;
+    cin >> a >> b >> n;
+    if(a==b || (a/n)>=b || n==1) cout<<1<<endl;
+    else{
+        cout<<2<<endl;
+    }
+}
+
+int main() {
+    fast_io
+    int T = 1;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
